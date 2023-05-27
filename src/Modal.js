791174ac79df1d9e3acf3864a3e46cbf2e19494a
Aaron Persaud <aaronpersaud@hotmail.com>
2023-05-26 23:42:21 -0400

    added function for handling submitting name

--- src/Modal.js
+++ src/Modal.js
@@ -1,19 +1,44 @@
-const Modal = (props) => {
-    const closeModal = () => {
-        const element = document.getElementById("modal");
-        element.style.display = 'none';
-    }
+import { useState } from "react";
 
-    return (
-        <div id="modal" className="hidden fixed left-0 top-0 bg-cyan-200 z-1">
-            <p>Please enter your name:</p>
-            <input className="rounded-sm"/>
-            <div>
-            <button onClick={props.onSubmit} className="rounded-sm bg-indigo-200">Submit</button>
-            <button className="rounded-sm float-right bg-indigo-200" onClick={closeModal}>Cancel</button>
-            </div>
-        </div>
-    )
-}
+const Modal = (props) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const closeModal = () => {
+    const element = document.getElementById("modal");
+    element.style.display = "none";
+  };
+
+  const handleSubmit = (e) => {
+    if (inputValue === "") {
+      alert("Cannot be blank");
+    } else {
+      //call function in moves
+      setInputValue("");
+      closeModal();
+    }
+  };
+
+  return (
+    <div id="modal" className="hidden fixed left-0 top-0 bg-cyan-200 z-1">
+      <p>Please enter your name:</p>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        className="rounded-sm"
+      />
+      <div>
+        <button onClick={handleSubmit} className="rounded-sm bg-indigo-200">
+          Submit
+        </button>
+        <button
+          className="rounded-sm float-right bg-indigo-200"
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Modal;
