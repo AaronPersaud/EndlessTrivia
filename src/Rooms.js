c4104edb0b3d5a1e39be5acccb989da6da3fb25b
Aaron Persaud <aaronpersaud@hotmail.com>
2023-05-28 23:43:18 -0400

    started component for joining a room

---
+++ src/Rooms.js
@@ -0,0 +1,21 @@
+const Rooms = (props) => {
+  const startGame = () => {
+    alert("Start the game!");
+  };
+
+  console.log(props.game.players)
+
+  return (
+    <div className="bg-white">
+      <p>Players:</p>
+      <div>
+          {props.game.players.map((player) => (
+            <div>{player}</div>
+          ))}
+      </div>
+      <button className="rounded-sm bg-indigo-200" onClick={startGame}>Start</button>
+    </div>
+  );
+};
+
+export default Rooms;
