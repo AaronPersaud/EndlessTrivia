350c861a7ba125b27e78250bf08537160060d561
Aaron Persaud <aaronpersaud@hotmail.com>
2023-06-04 21:22:21 -0400

    added function that sends question to OpenAI API

--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const { Server, Origins} = require('boardgame.io/server');
 const { Trivia } = require('./Trivia')
+const { Configuration, OpenAIApi} = require("openai");
+const key = require('../.env')
+
 
 const server = Server({
     games: [Trivia],
     origins: [Origins.LOCALHOST],
 });
+
+async function askGPTQuestion(question) {
+    const configuration = new Configuration({
+        apiKey: key.OPENAI_API_KEY
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "user", content: question}],
+    });
+    console.log(completion.data.choices[0].message);
+}
 
 server.run(8000);
