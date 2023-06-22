9514c8e320c84ab33d3e2fa8f3a71e99c790eb4f
Aaron Persaud <aaronpersaud@hotmail.com>
2023-06-21 23:13:00 -0400

    enter key functionality and displaying question after submit and other styling changes for asking bot questions

--- src/components/ai/AskQuestion.js
+++ src/components/ai/AskQuestion.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 const AskQuestion = (props) => {
   const [inputValue, setInputValue] = useState("");
   const [answer, setAnswer] = useState();
+  const [question, setQuestion] = useState("");
   const config = "You only answer trivia questions. If the question is not a trivia question, respond with 'Enter a trivia question'";
 
-  const handleSubmit = (e) => {
+  const handleSubmit = () => {
     if (inputValue === "") {
       alert("Cannot be blank");
     } else {
+      setQuestion(inputValue);
       setAnswer("Loading answer...")
       props.askGPT(inputValue, config).then((response) => {
         setAnswer(response.text)
@@ -21,18 +23,26 @@ const AskQuestion = (props) => {
     <div className="flex flex-col justify-center items-center">
         <h1 className="text-5xl">TriviaBot</h1>
         <br/>
-      <h5>Ask me a trivia question:</h5>
-      <div>
+      <div className="justify-center">
         <input
+          type="text"
+          placeholder="Enter a trivia question..."
           className="rounded-sm"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
+          onKeyUp={e => {
+            if (e.key === 'Enter') {
+              handleSubmit();
+            } 
+          }}
         />
-        <button className="bg-indigo-200 rounded-sm" onClick={handleSubmit}>
+        <button className="rounded-sm bg-indigo-200 hover:bg-indigo-400" onClick={handleSubmit}>
           Submit
         </button>
-        {answer && <div>{answer}</div>}
       </div>
+      <br/>
+        <div><p><b>Q: {question}</b></p></div>
+        {answer && <div>A: {answer}</div>}
     </div>
   );
 };
