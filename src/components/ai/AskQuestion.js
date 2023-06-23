397c90fa1532176a6c8e6efa8def2deb0cd22ff3
Aaron Persaud <aaronpersaud@hotmail.com>
2023-06-22 23:29:01 -0400

    disable submit button when inputvalue is empty

--- src/components/ai/AskQuestion.js
+++ src/components/ai/AskQuestion.js
@@ -3,26 +3,23 @@ import { useState } from "react";
 const AskQuestion = (props) => {
   const [inputValue, setInputValue] = useState("");
   const [answer, setAnswer] = useState();
-  const [question, setQuestion] = useState("");
-  const config = "You only answer trivia questions. If the question is not a trivia question, respond with 'Enter a trivia question'";
+  const [question, setQuestion] = useState();
+  const config =
+    "You only answer trivia questions. If the question is not a trivia question, respond with 'Enter a trivia question'";
 
   const handleSubmit = () => {
-    if (inputValue === "") {
-      alert("Cannot be blank");
-    } else {
-      setQuestion(inputValue);
-      setAnswer("Loading answer...")
-      props.askGPT(inputValue, config).then((response) => {
-        setAnswer(response.text)
+    setQuestion(inputValue);
+    setAnswer("Loading answer...");
+    props.askGPT(inputValue, config).then((response) => {
+      setAnswer(response.text);
     });
-      setInputValue("");
-    }
+    setInputValue("");
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-        <h1 className="text-5xl">TriviaBot</h1>
-        <br/>
+    <div className="pt-20 flex flex-col justify-center items-center">
+      <h1 className="text-5xl">TriviaBot</h1>
+      <br />
       <div className="justify-center">
         <input
           type="text"
@@ -30,19 +27,23 @@ const AskQuestion = (props) => {
           className="rounded-sm"
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
-          onKeyUp={e => {
-            if (e.key === 'Enter') {
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
               handleSubmit();
-            } 
+            }
           }}
         />
-        <button className="rounded-sm bg-indigo-200 hover:bg-indigo-400" onClick={handleSubmit}>
+        <button
+          disabled={inputValue === ""}
+          className="rounded-sm bg-indigo-200 hover:bg-indigo-400"
+          onClick={handleSubmit}
+        >
           Submit
         </button>
       </div>
-      <br/>
-        <div><p><b>Q: {question}</b></p></div>
-        {answer && <div>A: {answer}</div>}
+      <br />
+      {question && <div><p><b>Q: {question}</b></p></div>}
+      {answer && <div>A: {answer}</div>}
     </div>
   );
 };
